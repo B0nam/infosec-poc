73c78fb0b9dbe0be80396bba41be5ba225affaad
Matheus-Zauza-Maschietto <matheus_maschietto@yahoo.com.br>
2025-02-18 22:00:16 -0300

    Solving some import problems

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [
     UserService,
