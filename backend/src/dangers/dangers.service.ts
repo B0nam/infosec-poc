a62cb1544d8bd6e836634cec092a8a1cebece55b
kauanrossii <kauan.rossi@hotmail.com>
2025-02-24 19:13:05 -0300

    feat: operações iniciais da entidade de ameaça

--- backend/src/dangers/dangers.service.ts
+++ backend/src/dangers/dangers.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDangerDto } from './dto/create-danger.dto';
 import { UpdateDangerDto } from './dto/update-danger.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Danger } from './entities/danger.entity';
+import { Repository } from 'typeorm';
+import { Company } from 'src/company/entities/company.entity';
+import { ForumDomain } from 'src/forum-domains/entities/forum-domain.entity';
 
 @Injectable()
 export class DangersService {
-  create(createDangerDto: CreateDangerDto) {
-    return 'This action adds a new danger';
+  constructor(
+    @InjectRepository(Danger) private readonly dangersRepository: Repository<Danger>,
+    @InjectRepository(Company) private readonly companiesRepository: Repository<Company>,
+    @InjectRepository(ForumDomain) private readonly forumDomainsRepository: Repository<ForumDomain>,
+  ) { }
+
+  async create(createDangerDto: CreateDangerDto) {
+    const company = await this.companiesRepository.findOneBy({ id: createDangerDto.companyId });
+    if (!company) {
+      throw new NotFoundException("There isn't a valid company with the selected id");
+    }
+
+    const forumDomain = await this.forumDomainsRepository.findOneBy({ id: createDangerDto.forumDomainId });
+    if (!forumDomain) {
+      throw new NotFoundException("There isn't a valid forum domain with the selected id");
+    }
+
+    const newDanger = this.dangersRepository.create({
+      isSolved: createDangerDto.isSolved,
+      company,
+      forumDomain,
+      identificationDate: createDangerDto.identificationDate
+    });
+
+    return await this.dangersRepository.save(newDanger);
   }
 
-  findAll() {
-    return `This action returns all dangers`;
+  async findAll() {
+    return await this.dangersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} danger`;
+  async findOne(id: number) {
+    return await this.dangersRepository.findOneBy({ id });
   }
 
-  update(id: number, updateDangerDto: UpdateDangerDto) {
-    return `This action updates a #${id} danger`;
+  async update(id: number, updateDangerDto: UpdateDangerDto) {
+    const danger = await this.findOne(id);
+    danger.identificationDate = updateDangerDto.identificationDate;
+    danger.isSolved = updateDangerDto.isSolved;
+
+    if (danger.forumDomain.id !== updateDangerDto.forumDomainId) {
+      const newForumDomain = await this.forumDomainsRepository.findOneBy({ id: updateDangerDto.forumDomainId });
+      danger.forumDomain = newForumDomain;
+    }
+    
+    return await this.dangersRepository.save(danger);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} danger`;
+  async remove(id: number) {
+    const danger = await this.dangersRepository.findOneBy({ id });
+    return await this.dangersRepository.remove(danger);
   }
 }
--- backend/src/dangers/dto/update-danger.dto.ts
+++ backend/src/dangers/dto/update-danger.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDangerDto } from './create-danger.dto';
-
-export class UpdateDangerDto extends PartialType(CreateDangerDto) {}
+export class UpdateDangerDto {
+    forumDomainId: number;
+    identificationDate: Date;
+    isSolved: boolean;
+};
